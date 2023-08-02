ccea37bf597b730c9ed6db140921c14d1e271c1a
Mirau Pavel <miraupavel@gmail.com>
2023-08-02 11:22:13 +0700

    First commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 1_273_676;
+        boolean isPurchase = true;
+
+        int miles;
+        if (isPurchase) {
+            miles = 1;
+        } else {
+            miles = 0;
+        }
+
+        int bonus = price * miles / 20;
+        System.out.println("Бонусные милли: " + bonus);
+
+    }
+
+}
